0acbfb2dc65ccff7eb5bd56bcbdc9791c895480b
Brittany Burrows <brittany.burrows@gmail.com>
2014-06-24 10:39:07 -0700

    Added footer back in. Changed font for h1 tags. Added some anchor tags to move between sections.

--- script.js
+++ script.js
@@ -2,4 +2,15 @@ $(document).ready(function() {
 	$('#fullpage').fullpage({
 		anchors: ['home', 'design', 'chocolate', 'about'],
 	});
+
+	$(window).scroll(function () {
+		console.log($(this).scrollTop());
+        if ($(this).scrollTop() < 20) {
+            $('.navbar').removeClass('solidNav');
+        } else {
+            $('.navbar').addClass('solidNav');
+        }
+    });
+
+
 });
