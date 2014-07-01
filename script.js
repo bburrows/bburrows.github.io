f4c6e8291cef65c6cbb7adfcd42b1a0a41eaf949
Brittany Burrows <brittany.burrows@gmail.com>
2014-06-30 19:39:31 -0700

    Removed fullPage version of the site. Replaced with an alternating fixed/scroll version. Added first implementation of portfolio filter.

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
 $(document).ready(function() {
 
-	$('#fullpage').fullpage({
-		anchors: ['home', 'design', 'chocolate', 'about'],
-        autoScrolling: true,
-        scrollingSpeed: 500,
-	});
-
-
-
-	$(window).scroll(function () {
-		console.log($(this).scrollTop());
-        if ($(this).scrollTop() < 20) {
-            $('.navbar').removeClass('solidNav');
-        } else {
-            $('.navbar').addClass('solidNav');
-        }
-    });
-
 
 });
