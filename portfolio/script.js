1f7f328d94553fd30aa3732793c122e828c165d0
Brittany Burrows <brittany.burrows@gmail.com>
2014-07-09 15:02:10 -0700

    Menu changes color on scroll on landing page. Portfolio aitems added to /portfolio. Added gallery lightbox functionality to /portfolio.

---
+++ portfolio/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    $('#myLightbox').lightbox({
+
+        backdrop: true,
+        keyboard: true,
+        show: true,
+
+    });
+
+
+});
--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 $(document).ready(function() {
+    $('#target-nav li a').click(function() {
+        $(window).scrollTo($(this).attr('href'), 1500);
+    });
 
-
+    var designContainer = $('#design').position().top;
+    var chocolateContainer = $('#chocolate').position().top;
+    var aboutContainer = $('#about').position().top;
+    $(window).scroll(function() {
+        var windowOffset = $(window).scrollTop();
+        if (windowOffset < designContainer) {
+            $('#design-nav').removeClass('select');
+            $('#chocolate-nav').removeClass('select');
+            $('#about-nav').removeClass('select');
+        }
+        if (windowOffset >= designContainer && windowOffset < chocolateContainer) {
+            $('#design-nav').addClass('select');
+            $('#chocolate-nav').removeClass('select');
+            $('#about-nav').removeClass('select');
+        }
+        if(windowOffset >= chocolateContainer && windowOffset < aboutContainer) {
+            $('#design-nav').removeClass('select');
+            $('#chocolate-nav').addClass('select');
+            $('#about-nav').removeClass('select');
+        }
+        if (windowOffset >= aboutContainer - 30) {
+            $('#design-nav').removeClass('select');
+            $('#chocolate-nav').removeClass('select');
+            $('#about-nav').addClass('select');
+        }
+    });
 });
